b896f208f1dd33fc99d51e2873fea583ca76689d
Explosion <metewww40@gmail.com>
2026-06-03 19:42:06 +0300

    Initial commit: 197 bytes of pure machine code magic

---
+++ install.sh
@@ -0,0 +1,9 @@
+
+#!/bin/sh
+# Made by Exploder btw (i don giv a singl fuc abt credit u may remov dis)
+echo "installing boomshell..."
+xxd -r -p boomshell.hex boomshell
+chmod +x boomshell
+# Honestly, what the actual fuck did you expect from a install scrpt
+# for a 197 byte program?
+echo "You are ready to boom! just do './boomshell'" 
